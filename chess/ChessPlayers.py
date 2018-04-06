cf1b52d47d98e9a7cae62145b9185bd8efa0bf46
Sam Cohen <samsam489@gmail.com>
2018-04-06 17:07:11 -0400

    human player functionality

--- chess/ChessPlayers.py
+++ chess/ChessPlayers.py
@@ -12,6 +12,52 @@ class RandomPlayer():
             a = np.random.randint(self.game.getActionSize())
         return a
 
+class HumanChessPlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        # display(board)
+        #Collect valid moves for this position
+        valid = self.game.getValidMoves(board, 1)
+        for i in range(len(valid)):
+            if valid[i]:
+                print(int(i/self.game.n), int(i%self.game.n))
+        
+        #Grab human move and play it if valid
+        while True:
+            print("Enter an action: [moveFrom moveTo promotion-if-applicable]\n")
+            a = input()
+
+            a_split = a.split()
+
+            moveFrom, moveTo = a_split[0], a_split[1]
+
+            #b7-> b8
+
+            #check if the player entered a promotion out of [Knight: 'n', Bishop: 'b', Rook: 'r', Queen: 'q']
+            if len(a_split) == 3:
+                promotion = a_split[2]
+            else:
+                promotion = None
+
+            
+
+
+            action = self.game.n * 
+
+
+            x,y = [int(x) for x in a.split(' ')]
+            a = self.game.n * x + y if x!= -1 else self.game.n ** 2
+            if valid[a]:
+                break
+            else:
+                print('Invalid')
+
+        return a
+
+
+
 
 class HumanOthelloPlayer():
     def __init__(self, game):
