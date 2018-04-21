996fd5f6c88f2bff96134a8f882c33adcfe74ecb
Jason Zhao <zhao.s.jason@gmail.com>
2018-04-21 04:35:11 +0000

    Removing commented out merge conflicts in pit.py

--- pit.py
+++ pit.py
@@ -18,36 +18,22 @@ g = ChessGame()
 rp = RandomPlayer(g).play
 #gp = GreedyOthelloPlayer(g).play
 #hp = HumanChessPlayer(g).play
-abp = AlphaBetaPlayer(g).play
+#abp = AlphaBetaPlayer(g).play
 
 # nnet players
-# n1 = NNet(g)
-# n1.load_checkpoint('saves/save-bc5a3cffa65','temp.pth.tar')
-# args1 = dotdict({'numMCTSSims': 50, 'cpuct': 4.0})
-# mcts1 = MCTS(g, n1, args1)
-# n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
-# =======
-# n1 = NNet(g)
-# n1.load_checkpoint('saves/save-bc5a3cffa65','temp.pth.tar')
-# args1 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
-# mcts1 = MCTS(g, n1, args1)
-# n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+n1 = NNet(g)
+n1.load_checkpoint('saves/save-bc5a3cffa65','temp.pth.tar')
+args1 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
+mcts1 = MCTS(g, n1, args1)
+n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 # #n1p = lambda x: np.argmax(n1.predict(x))
-# >>>>>>> 04df74878f4b5a5565374e3972137ab4bfd66a54
 
-# n2 = NNet(g)
-# n2.load_checkpoint('saves/save-bc5a3cffa65','temp.pth.tar')
-# args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
-# mcts2 = MCTS(g, n2, args2)
-# n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
+n2 = NNet(g)
+n2.load_checkpoint('saves/save-bc5a3cffa65','temp.pth.tar')
+args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
+mcts2 = MCTS(g, n2, args2)
+n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
 
-# <<<<<<< HEAD
-#n2 = NNet(g)
-#n2.load_checkpoint('/dev/8x50x25/','best.pth.tar')
-#args2 = dotdict({'numMCTSSims': 25, 'cpuct':1.0})
-#mcts2 = MCTS(g, n2, args2)
-#n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
-
-arena = Arena.Arena(abp, rp, g, display=display)
+arena = Arena.Arena(n1p, n2p, g, display=display)
 
 print(arena.playGames(2, verbose=True))
